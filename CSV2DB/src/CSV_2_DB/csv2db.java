3fe54ca77d42c23841cd01dbca9a84de253802d6
avirupdebnath <avirup.debnath@gmail.com>
2018-04-14 15:31:05 +0530

    parsing a csv file in java and putting the values obtained from the csv file to a table

---
+++ CSV2DB/src/CSV_2_DB/csv2db.java
@@ -0,0 +1,34 @@
+package CSV_2_DB;
+import connectionDB.myDBConnectorUpdater;
+import csvOps.csvToArray;
+import java.sql.Connection;
+
+public class csv2db {
+
+	public static void main(String[] args) {
+		
+		//variables required
+		String fileurl="src\\data\\mydata.csv"; 	//csv file location
+		String DBurl="jdbc:mysql://sql12.freesqldatabase.com/sql12232496"; //db location
+		String user="sql12232496";		
+		String pass="19971207";
+		String tableName="Student";			//db table name
+		String columnNames="Name,Contact_No,Address";	//table column heads
+		
+		//no. of rows to be updated
+		int rows=csvToArray.getLines(fileurl);
+		System.out.println("Total no. of rows requesting update:"+rows);
+		
+		//get each row
+		String columns[]=csvToArray.getColumns();
+		
+		//connect to th db table and update
+		try {
+			Connection conn=myDBConnectorUpdater.getConnection(DBurl, user, pass);
+			myDBConnectorUpdater.insert(conn, tableName, columnNames, columns, rows);
+		}
+		catch(Exception e) {System.out.println(e);}
+
+	}
+
+}
